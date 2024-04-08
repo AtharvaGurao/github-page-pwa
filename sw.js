fb34a8dcf22ff0ad6d0a2ffecc7b14805b01ae59
AtharvaGurao <140331187+AtharvaGurao@users.noreply.github.com>
2024-04-08 09:19:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,24 @@
+
+// Change this to your repository name
 var GHPATH = '/github-page-pwa';
+
+// Choose a different app prefix name
 var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_002';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/css/styles.css`,
-  `${GHPATH}/img/icon.png`,
   `${GHPATH}/js/app.js`
 ]
-
-var CACHE_NAME = APP_PREFIX + VERSION
-self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { 
-        console.log('Responding with cache : ' + e.request.url);
-        return request
-      } else {       
-        console.log('File is not cached, fetching : ' + e.request.url);
-        return fetch(e.request)
-      }
-    })
-  )
-})
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
